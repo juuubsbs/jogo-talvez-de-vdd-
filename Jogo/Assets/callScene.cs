cd939c7d76231294712de7c0f736a02a72676d4f
juuubs_bs <julianasimon@edu.univali.br>
2024-11-19 16:47:05 -0300

    chama cena eeeeee

---
+++ Jogo/Assets/callScene.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class callScene : MonoBehaviour
+{
+    private Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(ChamaCena);
+    }
+
+    void ChamaCena(){
+        Debug.Log("Chamando cena");
+        SceneManager.LoadScene("MiniGame1");
+        SceneManager.UnloadSceneAsync("MiniGame1");
+    }
+}
